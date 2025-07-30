c4e87e6d877b508a86ee772fd0dd785c423117d6
gnanikasurabhi <gnanikasurabhi3056@gmail.com>
2025-07-30 10:40:23 +0530

    Add files via upload

---
+++ stringImmutability.java
@@ -0,0 +1,18 @@
+package oops_skill;
+
+public class stringImmutability {
+	public static void main(String[] args) {
+
+        String s1 = "data";
+        String s2 = s1;            
+        s1 = s1.concat("base");  
+             System.out.println(s1); 
+        String s3="hello";
+        s3.concat("world");
+             System.out.println("immutability"+" " +s3); 
+        
+
+     
+    }
+
+}
---
+++ stringSearchindex.java
@@ -0,0 +1,15 @@
+package oops_skill;
+
+public class stringSearchindex {
+	
+	 public static void main(String[] args) {
+	 String s = "Hello_world";
+	        System.out.println(s.indexOf("_"));        
+	        System.out.println(s.lastIndexOf("or"));    
+	        System.out.println(s.contains("ll"));      
+	    
+	        
+	 }
+
+
+}
